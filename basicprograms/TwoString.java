0a87b128cf4610fc0903ccc817e7c9777a3b33d8
S-Mythili <168315548+S-Mythili@users.noreply.github.com>
2025-07-09 21:26:01 +0530

    Update TwoString.java

--- basicprograms/TwoString.java
+++ basicprograms/TwoString.java
@@ -1,10 +1,5 @@
-package basicprograms;
-
 public class TwoString {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(args[0]+" Technologies "+args[1]);
 	}
-
 }
