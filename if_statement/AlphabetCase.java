49c090b2bb98f41ef7ebdc4903bab871db6cdc08
S-Mythili <168315548+S-Mythili@users.noreply.github.com>
2025-07-09 18:02:49 +0530

    Add files via upload

---
+++ if_statement/AlphabetCase.java
@@ -0,0 +1,16 @@
+package if_statement;
+import java.util.*;
+public class AlphabetCase {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char letter=sc.next().charAt(0);
+		if(letter>96) {
+			System.out.println((char)(letter-32));
+		}else {
+			System.out.println((char)(letter+32));
+		}
+		sc.close();
+	}
+
+}
---
+++ if_statement/InterestPercentage.java
@@ -0,0 +1,21 @@
+package if_statement;
+import java.util.*;
+public class InterestPercentage {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your Gender ");
+		String gender=sc.nextLine();
+		System.out.println("Age");
+		int age=sc.nextInt();
+		if(gender.equals("Female")&&(age>0&&age<59)) {
+			System.out.println("8.2%");
+		}else if(gender.equals("Female")&&(age>=59&&age<101)) {
+			System.out.println("9.2%");
+		}else if(gender.equals("Male")&&(age>0&&age<59)) {
+			System.out.println("8.4%");
+		}else {
+			System.out.println("10.5%");
+		}
+		sc.close();
+	}
+}
