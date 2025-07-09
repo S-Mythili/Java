d9829f9f34d75a44734a58c22d653e19785821a5
S-Mythili <168315548+S-Mythili@users.noreply.github.com>
2025-07-09 18:01:46 +0530

    Add files via upload

---
+++ basicprograms/SumofTwoNum.java
@@ -0,0 +1,11 @@
+package basicprograms;
+
+public class SumofTwoNum {
+	public static void main(String[] nums) {
+		int num1=Integer.parseInt(nums[0]);
+		int num2=Integer.parseInt(nums[1]);
+		int sum=num1+num2;
+		System.out.println("The sum of "+num1+" and "+num2+" is "+sum);
+	}
+
+}
