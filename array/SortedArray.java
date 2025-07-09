14db96832c101b9f3b4b8e1c427496412e83b272
S-Mythili <168315548+S-Mythili@users.noreply.github.com>
2025-07-09 21:22:30 +0530

    Update SortedArray.java

--- array/SortedArray.java
+++ array/SortedArray.java
@@ -1,11 +1,7 @@
-package array;
-
 import java.util.Scanner;
-
 public class SortedArray {
-
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);
         int size=sc.nextInt();
         int arr[]=new int[size];
         for(int i=0;i<size;i++) {
